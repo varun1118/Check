4e481765be10ddab215f3a864321d7cce8a48dc9
varun1118 <varun2018sharmavk@gmail.com>
2020-05-22 18:52:15 +0530

    req

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app=express()
 
 const PORT =process.env.PORT || 4444
 
-app.use('/',express.static(__dirname+'/public'))
+app.get('/',(req,res)=>{
+    res.send('HELLO WORLD')
+})
 
 app.listen(PORT,()=>{
     console.log(`http://localhost:${PORT}`)
